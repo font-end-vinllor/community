9e09f9642a3db1c0e371005cebe89b64bf819b22
font-end-vinllor <364621561@qq.com>
2019-09-06 17:14:29 +0800

    from code to properties

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import life.majiang.community.dto.AccessTokenDto;
 import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,15 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String client_id;
+
+    @Value("${github.client.secret}")
+    private String client_secret;
+
+    @Value("${redirect_uri}")
+    private String redirect_uri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
